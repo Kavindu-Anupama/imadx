9ed838b10b60d5cdad65c970d50be039bac526d8
Kavindu-Anupama <kavindu.karunarathne@rhino-partners.com>
2023-07-18 16:39:40 +0530

    integrated postgreSQL

--- spring-boot/spring-boot/src/main/java/com/rhinopartners/springboot/api/ReqHttpController.java
+++ spring-boot/spring-boot/src/main/java/com/rhinopartners/springboot/api/ReqHttpController.java
@@ -1,10 +1,17 @@
 package com.rhinopartners.springboot.api;
 
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("api/v1/app")
 public class ReqHttpController {
+
+    @PostMapping
+    public void create() {
+    }
+
 }
 
